b089036960c4d20138bb005b8a5175d7b73b07df
crymal <charles.rymal@nortek.com>
2016-01-04 15:23:54 -0800

    auto commit

--- __init__.py
+++ __init__.py
@@ -9,37 +9,36 @@ class MyBlockParser(BlockProcessor):
 	RE = re.compile('^\?({.*})?\n')
 	
 	def test(self, parent, block):
-		print 'test',repr(block)
-		return bool(self.RE.search(block))
+	    #print 'test',repr(block)
+	    return bool(self.RE.search(block))
 		
 	def run(self, parent, blocks):
-		print
-		print 'run', repr(blocks)
-		#print 'state', self.parser.state
+	    #print 'run', repr(blocks)
+	    #print 'state', self.parser.state
 		
-                class_name = 'question'
+            class_name = 'question'
 
-		raw_block = blocks.pop(0)
+	    raw_block = blocks.pop(0)
 		
-		m = self.RE.search(raw_block)
+	    m = self.RE.search(raw_block)
 
-                if m.group(1):
+            if m.group(1):
                     j = json.loads(m.group(1))
                     #print "extra data", j
                     try:
                         class_name = j['class']
                     except: pass
 		
-		#print 'm', m.start(), m.end()
+	    #print 'm', m.start(), m.end()
 		
-		div = etree.SubElement(parent, 'div')
-                div.attrib['class'] = class_name
-		#print div.attrib
+	    div = etree.SubElement(parent, 'div')
+            div.attrib['class'] = class_name
+	    #print div.attrib
 		
-		self.parser.parseBlocks(div, [raw_block[m.end():]])
-		#blocks.insert(0, raw_block[m.end():])
+	    self.parser.parseBlocks(div, [raw_block[m.end():]])
+	    #blocks.insert(0, raw_block[m.end():])
 		
-		return False
+	    return False
 
 class MyExtension(Extension):
 	def extendMarkdown(self, md, md_globals):
